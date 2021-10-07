6dcef9c306a7ed166ce0b79bde25b018e0e42ebd
unknown <nikita.waara@hotmail.com>
2021-10-07 09:55:38 +0300

    saving changes

--- part2/final-app/src/App.js
+++ part2/final-app/src/App.js
@@ -26,15 +26,23 @@ const App = () => {
     ]
   }
 
+  const total = course.parts.reduce((s, p) => {
+    console.log("morjesta", s, p)
+    return s + p.exercises
+  }, 0)
+
   return (
     <div>
       <ul>
+        <h2>
         <Course course={course} />
+        </h2>
       </ul>
       <ul>
       {course.parts.map(course => <Course key={course.id}  course={course} />)}
       </ul>
       <ul>
+        <h4>total amount of exercises: {total}</h4>  
       </ul>
     </div>
   )
