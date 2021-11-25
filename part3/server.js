f19101232b70374a4f2c56670246694a4f00e7bb
unknown <nikita.waara@hotmail.com>
2021-11-25 18:22:47 +0200

    saving changes

--- part3/server.js
+++ part3/server.js
@@ -1,8 +1,35 @@
+//const http = require('http')
+//ottaa käyttöön Noden sisäänrakennetun web-palvelimen määrittelevän moduulin. 
+//Kyse on käytännössä samasta asiasta kuin mihin olemme selainpuolen koodissa tottuneet (import http from 'http'),
+// mutta syntaksiltaan hieman erilaisessa muodossa.
+
 const http = require('http')
 
+let notes = [
+    {
+        id: 1,
+        content: "HTML is easy",
+        date: "2021-11-25T17:30:31.098Z",
+        important: true
+    },
+    {
+        id: 2,
+        content: "browser can execute only JavaScript",
+        date: "2021-11-25T18:39:31.091Z",
+        important: false
+
+    },
+    {
+        id: 3,
+        content: "GET and POST are the most important methods of HTTP protocol",
+        date: "2021-11-25T18:20:14.098Z",
+        important: true
+    }
+]
+
 const app = http.createServer((request, response) => {
-    response.writeHead(200, {'content-type': 'text/plain'})
-    response.end('Hello Universe')
+    response.writeHead(200, {'content-type': 'application/json'})
+    response.end(JSON.stringify(notes))
 })
 
 
