e3518b9dfbf7be50e9a777e46a34917ff8b994c3
unknown <nikita.waara@hotmail.com>
2021-11-28 10:17:13 +0200

    saving changes

--- part3/server.js
+++ part3/server.js
@@ -3,7 +3,9 @@
 //Kyse on käytännössä samasta asiasta kuin mihin olemme selainpuolen koodissa tottuneet (import http from 'http'),
 // mutta syntaksiltaan hieman erilaisessa muodossa.
 
-const http = require('http')
+//const http = require('http')
+const express = require('express')
+const app = express()
 
 let notes = [
     {
@@ -27,10 +29,16 @@ let notes = [
     }
 ]
 
-const app = http.createServer((request, response) => {
-    response.writeHead(200, {'content-type': 'application/json'})
-    response.end(JSON.stringify(notes))
+app.get('/',(req, res)=>{
+    res.send('<h1>Hello  Universe<h1>')
 })
+
+app.get('/api/notes', (req, res)=> {
+    res.json(notes)
+})
+//const app = http.createServer((request, response) => {
+    //response.writeHead(200, {'content-type': 'application/json'})
+    //response.end(JSON.stringify(notes))})
 
 
 const PORT = 3001
